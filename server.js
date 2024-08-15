41781e3e6d3dbc286fc7808cee09aa4a554eb970
Ahmed-NasrEldeen <45627481+Ahmed-NasrEldeen@users.noreply.github.com>
2024-08-15 20:14:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/health', (req, res) => {
-	res.send({
-		status: 'OK',
-		timestamp: new Date().toISOString(),
-	});
+app.get("/health", (req, res) => {
+  res.send({
+    status: "OK",
+    timestamp: new Date().toISOString(),
+  });
 });
 
-app.use('*', (req, res) => {
-	res.send(
-		'<img width="50%" align="center" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo39HTqrjJ4NY_NWYz3nF-6AfzCEjG8QAbbHEqEnECRAgpzsl4sIb8Fne8i7QZQQOWj3I&usqp=CAU">'
-	);
+app.use("*", (req, res) => {
+  res.send(`
+        <div style="text-align:center;">
+            <img width="50%" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo39HTqrjJ4NY_NWYz3nF-6AfzCEjG8QAbbHEqEnECRAgpzsl4sIb8Fne8i7QZQQOWj3I&usqp=CAU">
+            <div style="font-size:24px; color:red;">Hell</div>
+        </div>
+    `);
 });
-
 app.listen(port, () => {
-	console.log(
-		`Demo app is up and listening to port: ${port}, PID: ${process.pid}`
-	);
+  console.log(
+    `Demo app is up and listening to port: ${port}, PID: ${process.pid}`
+  );
 });
